4c552869ce066154ba478794ab6f258496898e78
leesh7048 <tmdgus7048@naver.com>
2021-11-27 16:41:46 +0900

    search router

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import styles from "./app.module.css";
+import Search from "./components/search/search";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Search />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
