70553bff58ef98586814c551b5d2696f16dc7372
leesh7048 <tmdgus7048@naver.com>
2021-12-12 17:18:33 +0900

    라이엇 api 호출 axios사용

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./app";
+import LolApi from "./api/lolApi";
+import axios from "axios";
+
+console.log({ LolApi });
+
+console.log({ API_KEY: process.env.REACT_APP_LOL_API_KEY });
+const httpClient = axios.create({
+  baseURL: "https://kr.api.riotgames.com",
+  params: { api_key: process.env.REACT_APP_LOL_API_KEY },
+});
+console.log(LolApi);
+const lolApi = new LolApi(httpClient);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App lolApi={lolApi} />
   </React.StrictMode>,
   document.getElementById("root")
 );
