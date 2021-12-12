d949053c3dac7ca624f8cae6159ae9caa48bcc0f
leesh7048 <tmdgus7048@naver.com>
2021-12-12 17:19:25 +0900

    summoner id api 호출, router설정

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import styles from "./app.module.css";
 import Search from "./components/search/search";
+import Summoner from "./components/summoner/summoner";
 
-function App() {
+function App({ lolApi }) {
+  const [searchId, setSearchId] = useState([]);
+  const search = (query) => {
+    console.log({ query });
+    lolApi.summoner(query).then((id) => setSearchId(id));
+  };
+  console.log(searchId);
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Search />} />
+          <Route
+            path="/"
+            element={<Search lolApi={lolApi} onSearch={search} />}
+          />
+          <Route path="/" element={<Summoner />} />
         </Routes>
       </BrowserRouter>
     </div>
