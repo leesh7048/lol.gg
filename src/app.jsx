0a28a395dc68c7b4c961c948d53c6a09a37d7631
leesh7048 <tmdgus7048@naver.com>
2022-01-01 17:10:11 +0900

    .

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,15 @@
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import styles from "./app.module.css";
 import Search from "./components/search/search";
 import Summoner from "./components/summoner/summoner";
 
 function App({ lolApi }) {
-  const [searchId, setSearchId] = useState([]);
-  const search = (query) => {
-    console.log({ query });
-    lolApi.summoner(query).then((id) => setSearchId(id));
-  };
-  console.log(searchId);
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={<Search lolApi={lolApi} onSearch={search} />}
-          />
-          <Route path="/" element={<Summoner />} />
+          <Route path="/" element={<Search />} />
+          <Route path="/summoner" element={<Summoner lolApi={lolApi} />} />
         </Routes>
       </BrowserRouter>
     </div>
