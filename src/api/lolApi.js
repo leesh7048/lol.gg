75f34519d0b135c1ba78afeca314a90f72136e44
leesh7048 <tmdgus7048@naver.com>
2022-01-01 17:09:51 +0900

    rankInfo,matchinfo api add

--- src/api/lolApi.js
+++ src/api/lolApi.js
@@ -1,12 +1,43 @@
 export default class LolApi {
-  constructor(httpClient) {
-    this.lolApi = httpClient;
+  constructor(krHttpClient, asiaHttpClient) {
+    this.krLolApi = krHttpClient;
+    this.asiaLolApi = asiaHttpClient;
   }
 
   async summoner(id) {
-    const response = await this.lolApi.get(
+    const response = await this.krLolApi.get(
       `lol/summoner/v4/summoners/by-name/${id}`
     );
-    return response.data.id;
+
+    return response.data;
+  }
+
+  async rank(id) {
+    const response = await this.krLolApi.get(
+      `lol/league/v4/entries/by-summoner/${id}`
+    );
+
+    return response.data;
+  }
+
+  async matches(puuid) {
+    const response = await this.asiaLolApi.get(
+      `lol/match/v5/matches/by-puuid/${puuid}/ids`,
+      {
+        params: {
+          start: 0,
+          count: 5,
+        },
+      }
+    );
+
+    return response.data;
+  }
+
+  async matchInfo(leagueId) {
+    const response = await this.asiaLolApi.get(
+      `lol/match/v5/matches/${leagueId}`
+    );
+    return response.data;
   }
 }
