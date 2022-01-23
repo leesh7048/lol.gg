ed8ebef8b42e3a4a090a43f10115b2c25c342b8e
leesh7048 <tmdgus7048@naver.com>
2022-01-23 14:04:32 +0900

    리팩토링

--- src/pages/summonerPage/summonerPage.jsx
+++ src/pages/summonerPage/summonerPage.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router";
+import { useNavigate } from "react-router-dom";
+
 import Profile from "./components/profile/profile";
 import styles from "./summonerPage.module.css";
 
@@ -10,45 +12,91 @@ const SummonerPage = ({ lolApi }) => {
   const [summonerProfile, setSummonerProfile] = useState({});
   const [soloRank, setSoloRank] = useState({});
   const [teamRank, setTeamRank] = useState({});
-  const [leagueIds, setLeagueIds] = useState([]);
   const [matchInfo, setMatchInfo] = useState([]);
+  const [matchPage, setMatchPage] = useState(0);
+  const [error, setError] = useState();
 
   const location = useLocation();
+  const navigate = useNavigate();
+  const userName = location.search.split("=")[1];
 
-  const getSummonerInfo = async () => {
-    const id = await lolApi.summoner(location.search.split("=")[1]);
-    setSummonerProfile(id);
-    const rankInfo = await lolApi.rank(id.id);
+  const getManipulatedUserName = (userName) => {
+    if (userName.length === 2) return userName.split("").join(" ");
+    else return userName;
+  };
 
-    if (rankInfo[0] && rankInfo[0].queueType === "RANKED_SOLO_5x5") {
-      setSoloRank(rankInfo[0]);
-      setTeamRank(rankInfo[1]);
-    } else {
-      setTeamRank(rankInfo[0]);
-      setSoloRank(rankInfo[1]);
+  const getSummonerProfile = async () => {
+    try {
+      console.log(encodeURI(getManipulatedUserName(userName)));
+      const summoner = await lolApi.summoner(getManipulatedUserName(userName));
+      setSummonerProfile(summoner);
+    } catch (error) {
+      setError(error);
     }
-    const getleagueIds = await lolApi.matches(id.puuid);
-    setLeagueIds(getleagueIds);
+  };
 
-    const getMatchInfo = await Promise.all(
-      getleagueIds.map((id) => lolApi.matchInfo(id))
-    );
+  const getRankInfoByQueueType = (rankInfo, targetQueueType) =>
+    rankInfo.find(({ queueType }) => queueType === targetQueueType);
 
-    setMatchInfo(getMatchInfo);
+  const getRankInfo = async (id) => {
+    const rankInfo = await lolApi.rank(id);
+
+    const soloRank = getRankInfoByQueueType(rankInfo, "RANKED_SOLO_5x5");
+    const teamRank = getRankInfoByQueueType(rankInfo, "RANKED_FLEX_SR");
+    setSoloRank(soloRank);
+    setTeamRank(teamRank);
+  };
+
+  const addMatchCount = () => {
+    setMatchPage(matchPage + 1);
+  };
+
+  const gameModeHandle = (e) => {
+    console.log(e);
   };
 
   useEffect(() => {
-    getSummonerInfo();
-  }, [location]);
+    if (userName) {
+      getSummonerProfile();
+    } else {
+      navigate("/", { replace: true });
+    }
+  }, [userName]);
+
+  useEffect(() => {
+    if (!summonerProfile.puuid || !summonerProfile.id) return;
+
+    const getMatchInfo = async () => {
+      const leagueIds = await lolApi.matches(summonerProfile.puuid, matchPage);
+      const newMatchInfo = await Promise.all(
+        leagueIds.map((id) => lolApi.matchInfo(id))
+      );
+      setMatchInfo([...matchInfo, ...newMatchInfo]);
+    };
+
+    getRankInfo(summonerProfile.id);
+    getMatchInfo();
+  }, [summonerProfile, matchPage]);
 
   return (
     <div className={styles.container}>
-      <Profile summonerProfile={summonerProfile} />
-      <div className={styles.bar}></div>
-      <div className={styles.contents}>
-        <Rank soloRank={soloRank} teamRank={teamRank} />
-        <Match matchInfo={matchInfo} summonerProfile={summonerProfile} />
-      </div>
+      {error ? (
+        <div>등록되지 않은 소환사입니다. 오타를 확인 후 다시 검색해주세요.</div>
+      ) : (
+        <>
+          <Profile summonerProfile={summonerProfile} />
+          <div className={styles.bar}></div>
+          <div className={styles.contents}>
+            <Rank soloRank={soloRank} teamRank={teamRank} />
+            <Match
+              matchInfo={matchInfo}
+              summonerProfile={summonerProfile}
+              addMatchCount={addMatchCount}
+              gameModeHandle={gameModeHandle}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 };
