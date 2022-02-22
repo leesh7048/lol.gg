fde9c335db6d646d2ce5e0dbb9852240b275ef40
leesh7048 <tmdgus7048@naver.com>
2022-02-22 17:35:46 +0900

    searchPage 컴포넌트 분리

--- src/pages/searchPage/searchPage.jsx
+++ src/pages/searchPage/searchPage.jsx
@@ -1,39 +1,14 @@
-import React, { useRef } from "react";
-import { useNavigate } from "react-router-dom";
-
+import React from "react";
+import SearchForm from "../summonerPage/components/searchForm/searchForm";
 import styles from "./searchPage.module.css";
 
 const SearchPage = () => {
-  const inputRef = useRef();
-  const navigate = useNavigate();
-
-  const handleSearch = () => {
-    const value = inputRef.current.value;
-    navigate(`/summoner?userName=${value}`);
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    handleSearch();
-  };
-  const onKeyPress = (event) => {
-    if (event.key === "Enter") {
-      handleSearch();
-    }
-  };
   return (
     <main className={styles.main}>
       <span className={styles.title}>lol.gg</span>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <input
-          className={styles.input}
-          type="text"
-          ref={inputRef}
-          onKeyPress={onKeyPress}
-        />
-        <button type="submit" className={styles.searchBtn}>
-          검색
-        </button>
-      </form>
+      <div className={styles.searchFormBox}>
+        <SearchForm />
+      </div>
     </main>
   );
 };
