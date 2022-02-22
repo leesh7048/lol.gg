5f64fc910596dd993e22bfe451be495b76687d64
leesh7048 <tmdgus7048@naver.com>
2022-02-22 17:44:53 +0900

    match component분리

---
+++ src/pages/summonerPage/components/matches/matches.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./matches.module.css";
+
+import Match from "../match/match";
+
+const Matches = ({ matchInfo, summonerProfile, addMatchCount, lolApi }) => {
+  return (
+    <div className={styles.matches}>
+      {matchInfo.map((infos) => (
+        <Match
+          infos={infos}
+          summonerProfile={summonerProfile}
+          lolApi={lolApi}
+        />
+      ))}
+      <div>
+        <div className={styles.addMatchBtn} onClick={addMatchCount}>
+          더보기
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Matches;
