72030b9eef90c454ef2b8d3a8667b9f27ceef548
leesh7048 <tmdgus7048@naver.com>
2022-04-17 16:04:33 +0900

    add rankLoadingSpinner

--- src/pages/summonerPage/components/rank/rank.jsx
+++ src/pages/summonerPage/components/rank/rank.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./rank.module.css";
 import { TIER_IMG_URLS } from "./tierImgUrl";
 
-const Rank = ({ soloRank, teamRank }) => {
+const Rank = ({ soloRank, teamRank, rankLoading }) => {
   const soloEmptyRank = {
     leaguePoints: "",
     tier: "UnRanked",
@@ -38,56 +38,88 @@ const Rank = ({ soloRank, teamRank }) => {
   const soloTierImg = TIER_IMG_URLS[soloTier];
   const teamTierImg = TIER_IMG_URLS[teamTier];
 
+  const miniSeries = soloRank?.miniSeries?.progress.split("");
+
+  const winLoseStyle = {
+    W: "win",
+    L: "lose",
+    N: "none",
+  };
+
   return (
     <div className={styles.rank}>
       <div className={styles.soloRank}>
-        <div className={styles.soloRankImg}>
-          <img
-            className={styles.soloTierImg}
-            src={soloTierImg}
-            alt="soloTier"
-          />
-        </div>
-        <div className={styles.soloRankInfo}>
-          <span>솔로랭크</span>
-          <span>
-            {soloTier} {soloTierRank}
-          </span>
-          <span>{soloLp ? `${soloLp}LP` : ""}</span>
-          <span>{soloWins ? `${soloWins}승${soloLosses}패` : ""}</span>
-          <span>
-            {soloWins
-              ? `승률 ${Math.round(
-                  (soloWins / (soloWins + soloLosses)) * 100
-                )}%`
-              : ""}
-          </span>
-        </div>
+        {!rankLoading ? (
+          <>
+            <div className={styles.soloRankImg}>
+              <img
+                className={styles.soloTierImg}
+                src={soloTierImg}
+                alt="soloTier"
+              />
+            </div>
+            <div className={styles.soloRankInfo}>
+              <span>솔로랭크</span>
+              <span>
+                {soloTier} {soloTierRank}
+              </span>
+              <span>{soloLp ? `${soloLp}LP` : ""}</span>
+              <span>{soloWins ? `${soloWins}승${soloLosses}패` : ""}</span>
+              <span>
+                {soloWins
+                  ? `승률 ${Math.round(
+                      (soloWins / (soloWins + soloLosses)) * 100
+                    )}%`
+                  : ""}
+              </span>
+              {soloRank?.miniSeries && (
+                <>
+                  <span>승급전</span>
+                  <div className={styles.miniSeries}>
+                    {miniSeries.map((winLose) => (
+                      <div className={styles[winLoseStyle[winLose]]}>
+                        {winLose === "N" ? "" : winLose}
+                      </div>
+                    ))}
+                  </div>
+                </>
+              )}
+            </div>
+          </>
+        ) : (
+          <div className={styles.loading}></div>
+        )}
       </div>
 
       <div className={styles.teamRank}>
-        <div className={styles.teamRankImg}>
-          <img
-            className={styles.teamTierImg}
-            src={teamTierImg}
-            alt="teamTier"
-          />
-        </div>
-        <div className={styles.teamRankInfo}>
-          <span>자유랭크</span>
-          <span>
-            {teamTier} {teamTierRank}
-          </span>
-          <span>{teamLp ? `${teamLp}LP` : ""}</span>
-          <span>{teamWins ? `${teamWins}승${teamLosses}패` : ""}</span>
-          <span>
-            {teamWins
-              ? `승률 ${Math.round(
-                  (teamWins / (teamWins + teamLosses)) * 100
-                )}%`
-              : ""}
-          </span>
-        </div>
+        {!rankLoading ? (
+          <>
+            <div className={styles.teamRankImg}>
+              <img
+                className={styles.teamTierImg}
+                src={teamTierImg}
+                alt="teamTier"
+              />
+            </div>
+            <div className={styles.teamRankInfo}>
+              <span>자유랭크</span>
+              <span>
+                {teamTier} {teamTierRank}
+              </span>
+              <span>{teamLp ? `${teamLp}LP` : ""}</span>
+              <span>{teamWins ? `${teamWins}승${teamLosses}패` : ""}</span>
+              <span>
+                {teamWins
+                  ? `승률 ${Math.round(
+                      (teamWins / (teamWins + teamLosses)) * 100
+                    )}%`
+                  : ""}
+              </span>
+            </div>
+          </>
+        ) : (
+          <div className={styles.loading}></div>
+        )}
       </div>
     </div>
   );
