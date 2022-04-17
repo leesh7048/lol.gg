f07cacba57cd3be116b9527baf996db2f832375e
leesh7048 <tmdgus7048@naver.com>
2022-04-17 16:11:18 +0900

    tooltip 위치조정 , useLayoutEffect 사용

--- src/pages/summonerPage/components/items/items.jsx
+++ src/pages/summonerPage/components/items/items.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import styles from "./items.module.css";
 
 const Items = ({ itemNum, itemsInfo }) => {
   const [tooltipIsActive, setTooltipIsActive] = useState(false);
+  const [tooltipTop, setTooltipTop] = useState(false);
+  const itemRef = useRef();
+  const tooltipRef = useRef();
 
   const onMouseOver = () => {
     if (!itemsInfo[itemNum]?.name) return;
+    if (!tooltipRef.current) return;
     setTooltipIsActive(true);
   };
 
@@ -16,12 +20,23 @@ const Items = ({ itemNum, itemsInfo }) => {
   const itemDescMarkup = (description) => {
     return { __html: description };
   };
+  useLayoutEffect(() => {
+    if (tooltipIsActive) {
+      window.innerHeight <
+      tooltipRef.current.getBoundingClientRect().height +
+        itemRef.current.getBoundingClientRect().bottom
+        ? setTooltipTop(true)
+        : setTooltipTop(false);
+    }
+  }, [tooltipIsActive]);
 
+  console.log(tooltipTop);
   return (
     <div
       className={styles.item}
       onMouseOver={onMouseOver}
       onMouseLeave={onMouseLeave}
+      ref={itemRef}
     >
       {itemNum !== 0 ? (
         <img
@@ -32,9 +47,17 @@ const Items = ({ itemNum, itemsInfo }) => {
       ) : (
         ""
       )}
-
       <div
-        className={tooltipIsActive ? styles.openTooltip : styles.closeTooltip}
+        className={
+          tooltipIsActive
+            ? tooltipTop
+              ? styles.topOpenTooltip
+              : styles.bottomOpenTooltip
+            : styles.closeTooltip
+        }
+        ref={(node) => {
+          tooltipRef.current = node;
+        }}
       >
         <span className={styles.itemName}>{itemsInfo[itemNum]?.name}</span>
         <span className={styles.itemPlainText}>
