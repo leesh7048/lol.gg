8d6c7f0adc90091b7c71fcd964d60cc8c52361fe
leesh7048 <tmdgus7048@naver.com>
2022-04-17 16:03:42 +0900

    tooltip 위치조절

--- src/pages/summonerPage/components/runeSpell/runeSpell.jsx
+++ src/pages/summonerPage/components/runeSpell/runeSpell.jsx
@@ -1,26 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styles from "./runeSpell.module.css";
 
 const RuneSpell = ({ runeSpellInfo, runeDescMarkup }) => {
   const [tooltipIsActive, setTooltipIsActive] = useState(false);
+  const [tooltipTop, setTooltipTop] = useState(false);
+
+  const runeSpellRef = useRef();
+  const tooltipRef = useRef();
 
   const onMouseOver = () => {
+    if (!tooltipRef.current) return;
     setTooltipIsActive(true);
   };
   const onMouseLeave = () => {
     setTooltipIsActive(false);
   };
 
+  useEffect(() => {
+    if (tooltipIsActive) {
+      window.innerHeight <
+      tooltipRef.current.getBoundingClientRect().height +
+        runeSpellRef.current.getBoundingClientRect().bottom
+        ? setTooltipTop(true)
+        : setTooltipTop(false);
+    }
+  }, [tooltipIsActive]);
+
   return (
     <div
       className={styles.runeSpell}
       onMouseOver={onMouseOver}
       onMouseLeave={onMouseLeave}
+      ref={runeSpellRef}
     >
       <img className={styles.runeSpellImg} src={runeSpellInfo.imgUrl} alt="" />
 
       <div
-        className={tooltipIsActive ? styles.openTooltip : styles.closeTooltip}
+        className={
+          tooltipIsActive
+            ? tooltipTop
+              ? styles.topOpenTooltip
+              : styles.bottomOpenTooltip
+            : styles.closeTooltip
+        }
+        ref={(node) => {
+          tooltipRef.current = node;
+        }}
       >
         <span className={styles.runeSpellName}>
           {runeSpellInfo?.runeSpellName}
